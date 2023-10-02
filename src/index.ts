96d823bd9a2c144f9ef03269a8cfc2f98f034674
hissak <hassan.a.issak@gmail.com>
2023-10-01 21:36:44 -0400

    Define list form an dinput variables in index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,9 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-import { v4 as uuidv4 } from 'uuid';
-console.log(uuidv4());
+// import { v4 as uuidv4 } from 'uuid';
+
+const list = document.getElementById('list') as HTMLUListElement
+const form = document.getElementById('new-task-form') as HTMLFormElement | null
+const input = document.getElementById('new-task-title') as HTMLInputElement
+
